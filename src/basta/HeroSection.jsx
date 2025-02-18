74bc298e13034fd6ee14664952b095a7baa6beb1
chachi30 <t.chachi32@gmail.com>
2025-02-19 00:04:44 +0800

    Updated HeroSection.jsx

--- src/basta/HeroSection.jsx
+++ src/basta/HeroSection.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Image from './img_5732.png';
 
 function HeroSection() {
   return (
     <section id="hero">
-      <img src="img_5732.png" alt="Profile" className="profile-image" />
+      <img src={Image} alt="Profile" className="image" />
       <h1 className="hero-text">
         Hello! I'm <span className="hero-name">Charls Hermosa</span>, <br />Front-end Designer
       </h1>
